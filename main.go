0f8f5bcec3ed4b119a3affe56bf6eef969acd670
GongJiangHua <1914805653@qq.com>
2020-10-10 20:25:21 +0800

    电子信息存储平台第一次提交

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"DataCertPlatform/db_mysql"
+	_ "DataCertPlatform/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	//连接数据库
+	db_mysql.Connect()
+	beego.Run()
+}
+
