450c60ad1242d2bc8a75d2102eb81a3c253c2bc1
GongJiangHua <1914805653@qq.com>
2020-10-14 23:15:39 +0800

    1014电子数据认证平台数据上传

--- routers/router.go
+++ routers/router.go
@@ -6,10 +6,14 @@ import (
 )
 
 func init() {
-	//路由
+	//router: 路由
     beego.Router("/", &controllers.MainController{})
+	//用户注册接口
     beego.Router("/register",&controllers.RegisterControllers{})
+	//用户登录的接口
     beego.Router("/login",&controllers.LoginControllers{})
+	//请求直接登录的页面
     beego.Router("/login.html",&controllers.LoginControllers{})
+	//用户上传文件的功能
     beego.Router("/home",&controllers.UploadFileControllers{})
 }
