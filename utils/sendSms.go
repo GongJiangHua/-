219c1629a36a5e1a001ca0cced0fc060112d8ac6
GongJiangHua <1914805653@qq.com>
2020-11-04 20:12:39 +0800

    1104电子数据认证平台最后完善

---
+++ utils/sendSms.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"github.com/alibaba-cloud-sdk-go/services/dysmsapi"
+	"github.com/astaxie/beego/config"
+)
+
+/**
+	该函数用于发送一条短信
+	phone ：电话，用于接收验证码的手机
+	code:发送验证码的数字
+	templateType：模板类型
+ */
+type SmsCode struct {
+	Code string `json:"code"`
+}
+type SmsResult struct {
+	Bizid string
+	Code string
+	Message string
+	Requestid string
+} 
+
+func SendSms(phone string,code string,templateType string)(*SmsResult,error) {
+	config := beego.AppConfig
+	accessKey := config.String(accessKey)
+	acceaaKeySecret:=config.String(acceaaKeySecret)
+	client,err := dysmsapi.NewClientWithAccessKey("cn-hangzhou",accessKey,acceaaKeySecret)
+	if err != nil {
+		return nil,err
+	}
+	//batch 批量，批次
+	request := dysmaapi.CreateSendSmsRequest()
+	request.PhoneNumbers = phone//指定要发送的那个目标手机号
+	request.SignName = "线上餐厅"//签名信息
+	request.TemplateCode = templateType //指定短信模板
+	//{"code":"XXXXXX"}Json格式
+	smsCode := SmsCode{Code:code}
+	smsbytes,_ := json.Marshal(smsCode)
+	request.TemplateParam = string(smsbytes)//指定要发送的验证码
+	
+	response,err := client.SendSms(request)
+	if err != nil {
+		return nil,err
+	}
+	//Biz : business 商业，业务
+	smsResult = &SmsResult{
+		Bizid:     response.BizId,
+		Code:      response.Code,
+		Message:   response.Message,
+		Requestid: response.RequestId,
+	}
+	return smsResult,nil
+}
+
+func GenRandCode(width int)string  {
+
+}
